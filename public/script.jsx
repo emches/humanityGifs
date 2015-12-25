0440463026719fe6d64c9b47b659279527e6735b
emilyches <emilychesler@gmail.com>
2015-12-25 13:18:41 -0500

    qcard stays now... not totally sure why

--- public/script.jsx
+++ public/script.jsx
@@ -1,13 +1,40 @@
 var QuestionFrame = React.createClass({
+
   render: function() {
     var qCard = this.props.qCard
 
+    console.log("rerendering question form");
+
     return (
+
         <div id="blackcard">
             <div className="cardText black">
             {qCard}
             </div>
         </div>
+    );
+  }
+});
+
+var GifsFrame = React.createClass({
+
+  render: function() {
+    var cards = this.props.cards.map(function(card) {
+      console.log("rerendering gifs frame");
+      return (    <div id="whitecard">
+            <div className="cardText white">
+            <img className="cardgif" height="100%" width="100%" src={card}/>
+            </div>
+        </div> );
+    });
+
+
+    return (
+      <div>
+       <br></br>
+        {cards}
+      <br></br>
+      </div>
     );
   }
 });
@@ -20,6 +47,7 @@ var Form = React.createClass({
     gifInput.value = '';
   },
   render: function() {
+    console.log("rerendering Form form");
     return (
       <form onSubmit={this.handleSubmit}>
         <input placeholder="submit gif" ref="gif" />
@@ -29,72 +57,41 @@ var Form = React.createClass({
   }
 });
 
-var CardAdder = React.createClass({
+var Game = React.createClass({
   getInitialState: function() {
-    return { myCards: [ "http://static1.purebreak.com.br/articles/7/73/97/@/39324-beyonce-esta-fazendo-33-anos-620x0-1.jpg"]
-    , qCards: ["When someone texts back 'k'", "When Netflix asks if you're still there", "When you're about to leave but your song comes on"]
-    //randomQ: this.pickRandomQ()
+    var startingQs= ["When someone texts back 'k'", "When Netflix asks if you're still there", "When you're about to leave but your song comes on"];
+
+    return {
+      myCards: [],
+      qCards: ["When someone texts back 'k'", "When Netflix asks if you're still there", "When you're about to leave but your song comes on"],
+      testQ: startingQs[Math.floor(Math.random()*(3 -0))]
     }
   },
-  addCard: function(testText) {
-    console.log("test text", this.state.myCards)
-    console.log("test text", this.props)
-    if ( testText.includes(".gif") || testText.includes(".jpg") ) {this.setState({myCards: this.state.myCards.concat( testText) }); }
-    else { alert("PLEASE ENTER A GIF");}
-    // this.setState({myCards: this.state.myCards.concat( Math.random()) });
+  pickQ: function(){
+  return this.state.qCards[Math.floor(Math.random()*(this.state.qCards.length -0))];
   },
-  // pickRandomQ: function() {
-  //   console.log("STATE", this)
-  //   return this.state.qCards[Math.floor(Math.random()*(this.state.qCards.length -0))]
-  // },
+  addCard: function(testText) {
+    console.log("testText", testText)
+    if ( testText.includes(".gif") || testText.includes(".jpg") ) {this.setState({myCards: this.state.myCards.concat( testText) }); }
+    else { alert("PLEASE ENTER A GIF");
+    }
+  },
+
   render: function() {
-    var cards = this.state.myCards.map(function(card) {
-      return (        <div id="whitecard">
-            <div className="cardText white">
-            <img className="cardgif" height="100%" width="100%" src={card}/>
-            </div>
-        </div> );
-    });
-    console.log("card length", cards.length)
-    var randomCard = this.state.qCards[Math.floor(Math.random()*(this.state.qCards.length -0))];
-    console.log("asf")
-    var randomGif = this.state.myCards[Math.floor(Math.random()*(this.state.myCards.length -0))];
-   // var myRandomQ = this.state.randomQ
-    //console.log("randomQ", randomQ)
+    var cards = this.state.myCards;
+    var pickQ = this.state.testQ;
+
     return (
       <div>
         <button onClick={this.addCard}>Test Button</button>
         <hr />
-      <QuestionFrame qCard={randomCard}/>
-
-       <br></br>
-        {cards}
-      <br></br>
+      <QuestionFrame qCard = {pickQ} />
+      <GifsFrame cards = {cards}/>
       <Form addCard={this.addCard}/>
       </div>
     )
   }
 });
 
-// var Main = React.createClass({
-//   getInitialState: function() {
-//     return {logins: []};
-//   },
-//   addCard: function(loginToAdd) {
-//     this.setState({logins: this.state.logins.concat(loginToAdd)});
-//   },
-//   render: function() {
-//     var cards = this.state.logins.map(function(login) {
-//       return (<Card login={login} />);
-//     });
-//     return (
-//       <div>
-//         <button onClick={this.addCard}>Test Button</button>
-//         <hr />
-//         {cards}
-//       </div>
-//     )
-//   }
-// });
 
-React.render(<CardAdder />, document.getElementById("root"));
+React.render(<Game />, document.getElementById("root"));
